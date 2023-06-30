7377e89952818fd222945358b052bc2212f9c1ef
Septian Maulana <septianm028@gmail.com>
2023-06-30 16:08:52 +0700

    feat: create modal for project form

--- components/index.ts
+++ components/index.ts
@@ -3,5 +3,15 @@ import Footer from "./Footer";
 import AuthProviders from "./AuthProviders";
 import Button from "./Button";
 import ProfileMenu from "./ProfileMenu";
+import Modal from "./Modal";
+import ProjectForm from "./ProjectForm";
 
-export { Navbar, Footer, AuthProviders, Button, ProfileMenu };
+export {
+  Navbar,
+  Footer,
+  AuthProviders,
+  Button,
+  ProfileMenu,
+  Modal,
+  ProjectForm,
+};
