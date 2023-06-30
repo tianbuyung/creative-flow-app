16bb7739638c5f78890c47353393c0c0deb7621c
Septian Maulana <septianm028@gmail.com>
2023-06-30 13:48:04 +0700

    feat: create profile menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
-import { signOut } from "next-auth/react";
 
 import { NavLinks } from "@/constants";
 import AuthProviders from "./AuthProviders";
 import { getCurrentUser } from "@/lib/session";
+import ProfileMenu from "./ProfileMenu";
 
 const Navbar = async () => {
   const session = await getCurrentUser();
@@ -35,23 +35,9 @@ const Navbar = async () => {
       <div className="flexCenter gap-4">
         {session?.user ? (
           <>
-            {session?.user?.image && (
-              <Link href={`/profile/${session?.user?.id}`} className="text-sm">
-                <Image
-                  src={session?.user?.image}
-                  width={40}
-                  height={40}
-                  className="rounded-full"
-                  alt={session?.user?.name}
-                />
-              </Link>
-            )}
+            <ProfileMenu session={session} />
 
             <Link href="/create-project">Share Work</Link>
-
-            <button type="button" className="text-sm" onClick={signOut}>
-              Sign out
-            </button>
           </>
         ) : (
           <AuthProviders />
--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,7 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import AuthProviders from "./AuthProviders";
+import Button from "./Button";
+import ProfileMenu from "./ProfileMenu";
 
-export { Navbar, Footer, AuthProviders };
+export { Navbar, Footer, AuthProviders, Button, ProfileMenu };
